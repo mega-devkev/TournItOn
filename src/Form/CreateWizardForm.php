3e3df3a6a9ee26d375fd09c956f7fffa52e551dd
ahamza <ahamzameto@gmail.com>
2024-01-15 13:35:37 +0100

    input für liste

--- src/Form/CreateWizardForm.php
+++ src/Form/CreateWizardForm.php
@@ -3,24 +3,26 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use App\Form\CreateWizardFlow;
 
 class CreateWizardForm extends AbstractType {
 
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-        $options['flow_step'] = 1;
+        $options['allow_extra_fields'] = true;
+        //dd($options);
 		switch ($options['flow_step']) {
 			case 1:
 				$builder
                     ->add('Name')
                     ->add('Location')
-                    ->add('Start-Date', DateTimeType::class, array(
+                    ->add('StartDate', DateTimeType::class, array(
                         'required' => true,
-                        'label' => 'form.label.datetime',
+                        'label' => 'Start Date',
                         'translation_domain' => 'AppBundle',
                         'attr' => array(
                             'class' => 'form-control input-inline datetimepicker',
@@ -28,23 +30,53 @@ class CreateWizardForm extends AbstractType {
                             'data-format' => 'dd-mm-yyyy HH:ii',
                         ),
                     ))
-                    ->add('End-Date', DateTimeType::class, array(
+                    ->add('EndDate', DateTimeType::class, array(
                         'required' => true,
-                        'label' => 'form.label.datetime',
+                        'label' => 'End Date',
                         'translation_domain' => 'AppBundle',
                         'attr' => array(
                             'class' => 'form-control input-inline datetimepicker',
                             'data-provide' => 'datepicker',
                             'data-format' => 'dd-mm-yyyy HH:ii',
                         ),
-                    ))
-                    ->add('submit', SubmitType::class);
+                    ));
 				break;
 			case 2:
+                $builder
+                    ->add('Logo', FileType::class, array(
+                        'required' => true,
+                        'label' => 'Upload a Logo',
+                    ))
+                    ->add('Mainsponsor', FileType::class, array(
+                        'required' => true,
+                        'label' => 'Upload the Logo for the Mainsponsor',
+                    ))
+                    ->add('Sponsors', FileType::class, array(
+                        'required' => true,
+                        'label' => 'Upload the Logo for other sponsors',
+                    ));
 				break;
             case 3:
+                $builder
+                    ->add('PlayerCard', ChoiceType::class, array(
+                        'choices' => [
+                            'test' => 'testValue',
+                        ],
+                        ))
+                    ->add('TextColor')
+                    ->add('Font')
+                    ->add('PrimaryColor')
+                    ->add('SecondaryColor')
+                    ->add('BackgroundImage', FileType::class, array(
+                        'required' => true,
+                        'label' => 'Upload a Background Image',
+                    ));
                 break;
             case 4:
+                $builder
+                    ->add('PlayerOrTeamName')
+                    ->add('Stats')
+                    ->add('Add', ButtonType::class);
                 break;
 		}
 	}
