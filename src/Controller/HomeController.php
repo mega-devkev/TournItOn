9974c15ed53a4fa3b55d64a260001ac80aef866f
kevblu <kevin@zwei.gmbh>
2024-01-15 08:17:52 +0100

    added Registration login and dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        if (!$this->getUser()) {
+            $userRoute = ['login', 'login'];
+        } else {
+            $userRoute = ['My Tournaments', 'tournament'];
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'route_type' => $userRoute,
         ]);
     }
 }
--- src/Security/LoginControllerAuthenticator.php
+++ src/Security/LoginControllerAuthenticator.php
@@ -2,11 +2,13 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -37,7 +39,7 @@ class LoginControllerAuthenticator extends AbstractLoginFormAuthenticator
             new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-                new RememberMeBadge(),
+//                new RememberMeBadge(),
             ]
         );
     }
@@ -49,8 +51,8 @@ class LoginControllerAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+         return new RedirectResponse($this->urlGenerator->generate('app_home'));
+//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
