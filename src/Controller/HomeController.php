99b6789079fcfa1120b22b67937fc37d7692b02f
kevblu <kevin@zwei.gmbh>
2024-01-15 13:37:56 +0100

    added new design + functions in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Tournament;
 use App\Repository\TournamentRepository;
-use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,23 +13,83 @@ class HomeController extends AbstractController
 {
 
     #[Route('/', name: 'app_home')]
-    public function index(TournamentRepository $tournamentRepository, UserRepository $userRepository): Response
+    public function index(TournamentRepository $tournamentRepository): Response
     {
-        $userAsArray = (array) json_decode($this->container->get('serializer')->serialize($this->getuser(), 'json'));
 
         if (!$this->getUser()) {
             $userRoute = ['login', 'login'];
-            $tournaments = $tournamentRepository->findAll();
 
         } else {
-            $userRoute = ['My Tournaments', 'tournament'];
-            $tournaments = $tournamentRepository->findBy(['userId' => $userAsArray['id']]);
+            $userRoute = ['My Tournaments', 'tournaments'];
         }
 
         return $this->render('home/index.html.twig', [
             'route_type' => $userRoute,
+            'tournaments' => $tournaments = $tournamentRepository->findAll()
+        ]);
+    }
+
+    #[Route('/detail/{id}', name: 'app_detail')]
+    public function details(TournamentRepository $tournamentRepository, Request $request): Response
+    {
+        $detailId = $request->attributes->get('id');
+
+        $tournament = $tournamentRepository->findOneBy(['id' => $detailId]);
+
+        return $this->render('home/detail.html.twig', [
+            'tournament' => $tournament
+        ]);
+    }
+
+    #[Route('/settings/{id}', name: 'app_settings')]
+    public function settings(TournamentRepository $tournamentRepository): Response
+    {
+        $userAsArray = (array) $this->searchUser();
+        $tournaments = $tournamentRepository->findBy(['userId' => $userAsArray['id']]);
+
+        return $this->render('home/settings.html.twig', [
+            'tournaments' => $tournaments
+        ]);
+    }
+    #[Route('/tournaments', name: 'app_tournaments')]
+    public function myTournaments(TournamentRepository $tournamentRepository): Response
+    {
+        $userAsArray = (array) $this->searchUser();
+
+        $tournaments = $tournamentRepository->findBy(['userId' => $userAsArray['id']]);
+
+        return $this->render('home/myTournaments.html.twig', [
+            'route_type' =>['My Tournaments', '#'],
             'tournaments' => $tournaments
         ]);
     }
 
+    #[Route('/tournaments/new', name: 'app_new_tournaments')]
+    public function newTournament(Request $request, TournamentRepository $tournamentRepository): Response
+    {
+        $user = new Tournament();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+            // TODO: redirect ändern
+            return $this->redirectToRoute('app_home');
+        }
+    }
+
+    private function searchUser()
+    {
+        return json_decode($this->container->get('serializer')->serialize($this->getuser(), 'json'));
+    }
 }
