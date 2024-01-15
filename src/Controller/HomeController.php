ac81948c10baa0f5d4073814c29846c17dac9ee4
kevblu <kevin@zwei.gmbh>
2024-01-15 11:17:52 +0100

    bilder in richtigen ordner

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,33 @@
 
 namespace App\Controller;
 
+use App\Repository\TournamentRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TournamentRepository $tournamentRepository, UserRepository $userRepository): Response
     {
+        $userAsArray = (array) json_decode($this->container->get('serializer')->serialize($this->getuser(), 'json'));
+
         if (!$this->getUser()) {
             $userRoute = ['login', 'login'];
+            $tournaments = $tournamentRepository->findAll();
+
         } else {
             $userRoute = ['My Tournaments', 'tournament'];
+            $tournaments = $tournamentRepository->findBy(['userId' => $userAsArray['id']]);
         }
 
         return $this->render('home/index.html.twig', [
             'route_type' => $userRoute,
+            'tournaments' => $tournaments
         ]);
     }
+
 }
